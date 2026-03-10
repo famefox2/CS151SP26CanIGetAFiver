09c953650d347955f1c2207ac5b712dbfc4c70d2
famefox2 <54617980+famefox2@users.noreply.github.com>
2026-03-09 19:35:37 -0700

    Update GameRPSHuVsCo.java

--- RockPaperScissors/src/GameRPSHuVsCo.java
+++ RockPaperScissors/src/GameRPSHuVsCo.java
@@ -1,9 +1,15 @@
+/**
+ * Rock, paper, scissors for a human vs a computer
+ */
 public class GameRPSHuVsCo extends Game{
 
     private Player human;
     private Player computer;
     private final static int ROUNDS = 20;
     
+    /**
+     * Make the RPS game for 20 rounds. Creates the human and computer players, the game rules for the game
+     */
     @Override
     public void runGame(){
         int count = 1;
