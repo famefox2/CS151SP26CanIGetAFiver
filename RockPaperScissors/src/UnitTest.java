78bd2d24a0eda434c60ac2ff33e0940f3e12f3e2
thrtn70 <xuanthong.thornton@gmail.com>
2026-03-09 21:02:13 -0700

    Sign enum test and 1 Computer test added

--- RockPaperScissors/src/UnitTest.java
+++ RockPaperScissors/src/UnitTest.java
@@ -1,4 +1,21 @@
 public class UnitTest {
+
+    // Sign enum tests
+    static void testSignValues(){
+        assert Sign.ROCK.getValue()==0: "ROCK should have value 0";
+        assert Sign.PAPER.getValue()==1: "PAPER should have value 1";
+        assert Sign.SCISSORS.getValue()==2: "SCISSORS should have value 2";
+        System.out.println("PASSED: testSignValues");
+     }
+
+    // Computer tests
+    static void testComputerChoiceNotNull(){
+        Computer comp = new Computer(42);
+        Sign choice=comp.makeChoice();
+        assert choice !=null: "Computer makeChoice should not return null";
+        System.out.println("PASSED: testComputerChoiceNotNull");
+    }
+
     public static void main(String[] args) {
 
 
@@ -8,3 +25,4 @@ public class UnitTest {
 
     }
 }
+
