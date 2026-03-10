99d1313b78ff63f656f9cd14f5233729005ad899
famefox2 <54617980+famefox2@users.noreply.github.com>
2026-03-09 19:36:24 -0700

    Update HumanPlayer.java

--- RockPaperScissors/src/HumanPlayer.java
+++ RockPaperScissors/src/HumanPlayer.java
@@ -6,18 +6,27 @@ public class HumanPlayer extends Player {
         super.setPlayerName("Human");
         humanInput = new HumanInput();
     }
-
+    /**
+     * Gets human player choice from command line and save it
+     */
     @Override
     public void getintRPS(){
         int input;
         input = humanInput.inputThrow();
         intToEnum(input);
     }
+    /**
+     * Get the player's choice and return their choice
+     */
     @Override
     public Sign getCurSign(){
         getintRPS();
         return super.getCurSign();
     }
+    /**
+     * Return the human player's choice. 
+     * @return the sign human player has chosen
+     */
     @Override
     public Sign makeChoice(){
         getintRPS();
